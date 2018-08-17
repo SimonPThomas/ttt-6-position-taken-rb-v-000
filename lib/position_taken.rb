684a3c0f8cc2d7ea9cd89e5869674bffab0a046c
Simon Thomas <simonpthomas89@gmail.com>
2018-08-17 15:36:06 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,12 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = [0]
 
 def position_taken? (board, index)
+
 board(index)
 
 end
-
+if position_taken? == false
+  puts index
+end
 
 position_taken? (board, index)

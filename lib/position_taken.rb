9aaa5474e3c18c00659bd5cb2444ff5676522a0f
Simon Thomas <simonpthomas89@gmail.com>
2018-08-17 15:35:19 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+index = [0]
+
+def position_taken? (board, index)
+board(index)
+
+end
+
+
+position_taken? (board, index)
